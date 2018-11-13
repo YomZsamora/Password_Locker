5c5067f1d8ad84117cb047d31b7d6eef575432fa
yomz <samora.y@adzumi.co.ke>
2018-11-13 11:20:38 +0300

    Created test for finding user using Username.

--- user_test.py
+++ user_test.py
@@ -53,12 +53,26 @@ class TestUser(unittest.TestCase):
 		self.assertEqual(len(User.user_list),1)
 
 
-	def tearDown(save_user):
+	def tearDown(self):
 		'''
         tearDown method that does clean up after each test case has run.
         '''
 
 		User.user_list = []
+
+
+	def test_find_user_by_username(self):
+		'''
+		Test to find out if we can find user by username and display credentials
+		'''
+
+		self.new_user.save_user()
+		test_user = User("samora","password")
+		test_user.save_user()
+
+		found_user = User.find_by_username("samora")
+
+		self.assertEqual(found_user.password, test_user.password)
 
 
 if __name__ == '__main__':
